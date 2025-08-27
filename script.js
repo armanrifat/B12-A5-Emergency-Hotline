977d14941fd7cd688fa0279e467592fd99727d32
mac mini m1 <macminim1@macs-Mac-mini.local>
2025-08-27 07:31:52 +0600

    add DOM Elements

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 let heartCount = 0;
 let coinCount = 100;
 let copyCount = 0;
+
+// DOM Elements
+const heartCounter = document.getElementById("heartCount");
+const coinCounter = document.getElementById("coinCount");
+const copyCounter = document.getElementById("copyCount");
+const historyList = document.getElementById("historyList");
+const clearHistoryBtn = document.getElementById("clearHistory");
+const cardSection = document.getElementById("cardSection");
